47fddc222ba292c3743869bfd781b03fa40affb8
Joy Charles <joycharles926@gmail.com>
2026-04-15 08:10:54 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import AddProduct from './AddProduct';
+import GetProducts from './components/Getproduct';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId="882400013905-6im67kk4qulsrmhdqiecjih7oqhp4taf.apps.googleusercontent.com">
+      <BrowserRouter>
+        <div className="App">
+
+          {/* Optional header */}
+          <h1 style={{ textAlign: "center", margin: "20px 0" }}>
+            Welcome to EcoShop
+          </h1>
+
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/add-product" element={<AddProduct />} />
+            <Route path="/get-product" element={<GetProducts />} />
+          </Routes>
+
+        </div>
+      </BrowserRouter>
+
+    </GoogleOAuthProvider>
   );
 }
 
